06a693eab93c94cb00481a65909571ed00360336
apfdamascena <apfd@cin.ufpe.br>
2020-07-03 15:33:01 -0300

    training flexbox on react

---
+++ src/Components/Button/Button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Button.css';
+
+const Button = (props) =>{
+    return <button id="buttonComponent">{props.nome}</button>
+}
+
+export default Button;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Home from './Components/Home/Home';
+import Button from './Components/Button/Button';
+
+class App extends Component{
+  render(){
+    return(
+      <div className = "container">
+        <h2>oi meu nome é alex</h2>
+        <Home name="dasjdlkasdjlasjdklasjdlk" nomePai="penelope"/>
+        <div id="alex">
+          <Button id ="alex1"nome="Adicionar"/>
+          <Button id="alex" nome = "Menu"/>
+        </div>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App/>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+
